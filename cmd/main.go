d5c5fb87fc7d906254585bbb4fe7189d1a8c85a3
g0xu <keig0xurita@gmail.com>
2024-07-11 22:10:22 +0900

    Implementation of ask command (#3)
    * Implementation of ask command
    
    * Corrected error handling of Issue information retrieval
    
    * Update error handling when intent argument is empty

--- cmd/main.go
+++ cmd/main.go
@@ -16,6 +16,7 @@ func main() {
 		repo        = flag.String("repo", "", "Repository name")
 		owner       = flag.String("owner", "", "Repository owner")
 		issueNumber = flag.Int("issue", 0, "Issue number")
+		intent      = flag.String("intent", "", "Question or intent for the 'ask' command")
 		command     = flag.String("command", "", "Command to be executed by AI")
 		configFile  = flag.String("config", "./internal/config/config.yaml", "Configuration file")
 		gh_token    = flag.String("github-token", "", "GitHub token")
@@ -34,9 +35,6 @@ func main() {
 		log.Ldate|log.Ltime|log.Llongfile|log.Lmsgprefix,
 	)
 
-	// Pre-define variables for error handling
-	var err error
-
 	// Get configuration
 	cfg, err := utils.NewConfig(*configFile)
 	if err != nil {
@@ -45,10 +43,19 @@ func main() {
 
 	// Create a GitHub Issues instance. From now on, you can control GitHub from this instance.
 	issue := github.NewIssue(*owner, *repo, *issueNumber, *gh_token)
+	if issue == nil {
+		logger.Fatalf("Failed to create GitHub issue instance")
+	}
 
 	// Get Issue's information(e.g. Title, Body) and add them to the user prompt except for comments by Actions.
-	title, _ := issue.GetTitle()
-	body, _ := issue.GetBody()
+	title, err := issue.GetTitle()
+	if err != nil {
+		logger.Fatalf("Error getting Title: %v", err)
+	}
+	body, err := issue.GetBody()
+	if err != nil {
+		logger.Fatalf("Error getting Body: %v", err)
+	}
 	if cfg.System.Debug.Log_level == "debug" {
 		logger.Println("Title:", *title)
 		logger.Println("Body:", *body)
@@ -58,6 +65,9 @@ func main() {
 
 	// Get comments under the Issue and add them to the user prompt except for comments by Actions.
 	comments, _ := issue.GetComments()
+	if err != nil || comments == nil {
+		logger.Fatalf("Error getting comments: %v", err)
+	}
 	for _, v := range comments {
 		if *v.User.Login == "github-actions[bot]" {
 			continue
@@ -69,7 +79,18 @@ func main() {
 	}
 
 	// Set system prompt
-	system_prompt := cfg.Ai.Commands[*command].System_prompt
+	var system_prompt string
+    if *command == "ask" {
+        if *intent == "" {
+			log.SetOutput(os.Stdout)
+            logger.Println("Error: intent is required for 'ask' command")
+			flag.PrintDefaults()
+			os.Exit(1)
+        }
+        system_prompt = cfg.Ai.Commands[*command].System_prompt + *intent
+    } else {
+        system_prompt = cfg.Ai.Commands[*command].System_prompt
+    }
 	prompt := ai.Prompt{UserPrompt: user_prompt, SystemPrompt: system_prompt}
 	logger.Println("\x1b[34mPrompt: |\n", prompt.SystemPrompt, prompt.UserPrompt, "\x1b[0m")
 
